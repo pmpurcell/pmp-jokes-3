ba025142cf33fad10fdfd9ada97070510da8f7df
pmpurcell93 <maddenpurcell@gmail.com>
2021-10-06 21:28:53 -0500

    Rendered punchline and new joke

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -3,11 +3,14 @@ import getJoke from '../jokeData';
 
 function Initialize() {
   const [btnText, setBtnText] = useState('Get a Joke');
-  const [setupText, setSetupText] = useState('');
+  const [joke, setJoke] = useState('');
   console.warn(btnText, setBtnText);
   const btnFunction = () => {
     getJoke().then((response) => {
-      setSetupText(response.setup);
+      setJoke({
+        setup: response.setup,
+        delivery: response.delivery,
+      });
     });
     setBtnText('Get Punchline');
   };
@@ -16,8 +19,28 @@ function Initialize() {
     <div className="App">
       <div id="mainContainer">
         <h2>Jokes!</h2>
-        <h3>{setupText}</h3>
-        <button onClick={btnFunction} type="button" className="btn btn-dark">{btnText}</button>
+        <h3>{joke.setup}</h3>
+        <h4>{ btnText !== 'Get Punchline' ? joke.delivery : ''}</h4>
+        <div>
+          {btnText === 'Get a Joke' || btnText === 'Get a New Joke' ? (
+            <button
+              onClick={btnFunction}
+              type="button"
+              className="btn btn-dark"
+            >
+              {btnText}
+            </button>
+          ) : (
+            <button
+              onClick={() => setBtnText('Get a New Joke')}
+              type="button"
+              className="btn btn-dark"
+            >
+              {btnText}
+            </button>
+          )}
+          ;
+        </div>
       </div>
     </div>
   );
